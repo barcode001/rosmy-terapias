263b10a8a90d8ccaef45df1c6be0d774f4adfce4
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-08-22 12:03:36 -0500

    add compressed logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo/logo.png";
+import logo from "../assets/logo/logo.webp";
 import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 
